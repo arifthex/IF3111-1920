8291de46e32b1a0da4843a0a69d63c9e90e81684
Arif wicak <ariftheaxr@gmail.com>
2019-11-27 13:52:48 +0700

    add method model tombol search

--- application/models/Lapor_model.php
+++ application/models/Lapor_model.php
@@ -3,9 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Lapor_model extends CI_Model{
 	//Membuat model untuk mengambil database dari tabel laporan 
-    public function getLaporan(){    
-        $data = $this->db->get('laporan');
-        return $data->result_array();
-    }
+	public function getLaporan(){    
+		$data = $this->db->get('laporan');
+		return $data->result_array();
+	}
+	public function search(){  
+		$key = $this->input->post('keyword');
+
+		$this->db->like('judul',$key);
+		$this->db->or_like('isi',$key);
+		$data = $this->db->get('laporan');
+		return $data->result_array();  
+	}
 }
 ?>
