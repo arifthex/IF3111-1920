11cce2d7ec1a2a8ad5aaf8e5b8b2a339fc05a2a0
Arif wicak <ariftheaxr@gmail.com>
2019-11-27 13:53:52 +0700

    update susunan beranda
    menambahkan percabanagan hasil pencarian

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,35 +1,47 @@
+
 <!-- View pada Beranda Home Web  -->
-	<div id="badan">
-		<form action="" method="POST">
-			<input type="text" name="kata" style="height:2em; width: 70%" placeholder="Masukkan laporan yang ingin anda cari...">
-			<input type="submit" name="cari" value="Cari" style="height:3.15em; width:8%">
-		</form>
-		<a href="<?= base_url();?>home/form">Buat Laporan/komentar <img height="20px" src="<?= base_url()?>asset/icon/tambah.png"></a>
-	</div>
-	<div id="konten">
-		<br><br>
-		<p>Laporan/Komentar Terakhir </p>
-		<br><hr>
-			<?php foreach ($laporan as $value): ?>
+<div id="badan">
+	<form action="<?= base_url(); ?>Home/search" method="POST">
+		<input type="text" placeholder="Masukkan laporan yang ingin anda cari..." 
+		name="keyword" id="keyword" autocomplete="off">
+		<input type="submit" name="cari" value="Cari" id="cari-data">
+	</form>
+	<a href="<?= base_url();?>home/form">Buat Laporan/komentar <img height="20px" src="<?= base_url()?>asset/icon/tambah.png"></a>
+</div>
+<div id="konten">
+	<br><br>
+	<p>
+	<?php if (isset($name)) {
+		echo "$name";
+	} else {
+		echo "Laporan/Komentar Terakhir ";
+	}
+	?>
+	</p>
+	<br><hr>
+	<div id="view">
+		<?php foreach ($laporan as $value): ?>
 			<div class="wrapper">
 				<div class="gambar">
 					<img height="50px" src="<?= base_url()?>asset/icon/user.png">
 				</div>
 				<b>
-				<?= $value['author']; ?> <br>
+					<?= $value['author']; ?> <br>
 					<span>Kategori : <?= $value['kategori']; ?></span>
 				</b>
 			</div>
 			<h1><?= $value['judul']; ?></h1>
 			<br>
-			<p style="text-align: justify;">
-			<?= $value['isi']; ?>
+			<p>
+				<?= $value['isi']; ?>
 			</p>
 			<br>
 			<?= $value['lampiran']; ?> (ini lampiran)
 			<span>
-			<p><?= $value['waktu']; ?> &nbsp <b>Lihat Selengkapnya ></b></p>
+				<p><?= $value['waktu']; ?> &nbsp <b>Lihat Selengkapnya ></b></p>
 			</span>
 			<hr>
 		<?php endforeach; ?>
-	</div> 
+	</div>
+</div> 
+<script src="<?= base_url()?>asset/js/script.js"></script>
