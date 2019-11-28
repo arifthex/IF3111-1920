6577c0c61e957ca5c04ddfaab20fba0969a810b5
Arif wicak <ariftheaxr@gmail.com>
2019-11-28 13:51:35 +0700

    add hitung kata js

--- asset/js/validasi.js
+++ asset/js/validasi.js
@@ -1,10 +1,16 @@
 function validasi() {
-	var text = document.forms["data"]["laporan"].value
+	var text=document.forms["data"]["laporan"].value
 	var kategori = document.forms["data"]["aspek"].value
 	var lampiran = document.forms["data"]["lampiran"].value
-	
+
+	//Hitung banyak kata
+    var panjang=text.split(" ") 
+
 	if (text == "") {
 		alert("Isi laporan tidak boleh kosong")
+		return(false)
+	} else if (panjang.length < 20) {
+		alert("Isi laporan Minimal 20 kata")
 		return(false)
 	}
 	if (kategori == "NULL") {
