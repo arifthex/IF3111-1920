2d82861be93782a7056129f78c5f848b9ec05bce
Arif wicak <ariftheaxr@gmail.com>
2019-11-30 19:01:21 +0700

    Add input model
    Model untuk CRUD

--- application/models/Input_model.php
+++ application/models/Input_model.php
@@ -2,10 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Input_model extends CI_Model{
+	private $_table = "laporan";
 	//Membuat model untuk mengambil database dari tabel laporan 
-    public function insert(){    
-        $data = $this->db->get('laporan');
-        return $data->result_array();
+    public function tambah($data,$table){    
+        return $this->db->insert($table, $data);
     }
+
 }
 ?>
