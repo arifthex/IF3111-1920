5f552fc7d2ec3c969d5fff20c4e4ce105dad28f5
Arif wicak <ariftheaxr@gmail.com>
2019-11-30 21:44:24 +0700

    Penambahan fitur upload file ke database

--- application/controllers/CRUD.php
+++ application/controllers/CRUD.php
@@ -9,56 +9,6 @@ class CRUD extends CI_Controller {
 		$this->load->model('Input_model');
 	}
 	//Membuat controller Untuk Tambah data
-	public function create() {
-		$nama	= $this->input->post('author');
-		$isi	= $this->input->post('isi');
-		$judul	= $this->input->post('kategori');
-		$aspek	= $this->input->post('kategori');
-
-		$config['upload_path']          = 'upload/file/';
-		$config['allowed_types']        = 'gif|jpg|png';
-		$config['max_size']             = 1024;
-    	// $config['max_width']            = 1024;
-    	// $config['max_height']           = 768;
-
-		$this->load->library('upload', $config);
-
-		if ( !$this->upload->do_upload('lampiran')) {
-			$error = array('error' => $this->upload->display_errors());
-			$this->load->view('home/form', $error);
-		} else {
-			$this->upload->data('file_name');
-			$data = array(
-				'author' => $nama,
-				'isi' => $isi,
-				'kategori' => $judul,
-				'kategori' => $aspek
-			);
-			$this->Input_model->insert($data,'laporan');
-			redirect('Home/index');
-
-    	// $this->load->view('upload_success', $data); Tempat view success upload
-		}
-	}
-
-	public function add2() { //FIX
-		$nama	= $this->input->post('author');
-		$judul	= $this->input->post('judul');
-		$isi	= $this->input->post('isi');
-		$aspek	= $this->input->post('aspek');
-		$lampiran	= "img.doc";
-
-		$data = array(
-			'author' 	=> $nama,
-			'isi' 		=> $isi,
-			'kategori' 	=> $judul,
-			'kategori' 	=> $aspek,
-			'lampiran' 	=> $lampiran
-		);
-		$this->Input_model->tambah($data,'laporan');
-		redirect('Home/index');
-	}
-
 	public function add() {
 		$data['title']='Buat Laporan';
 		$nama	= $this->input->post('author');
@@ -71,13 +21,24 @@ class CRUD extends CI_Controller {
 		$config['max_size']             =  2048;
 
 		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload('lampiran')) {
+		//Ketika lampiran di upload
+		if (! $_FILES['lampiran']['error'] <> 4) {
+			$uploadData = $this->upload->data();
+			$data = array(
+				'author' 	=> $nama,
+				'isi' 		=> $isi,
+				'judul' 	=> $judul,
+				'kategori' 	=> $aspek,
+				'lampiran' 	=> $uploadData['file_name'] 
+			);
+			$this->Input_model->tambah($data,'laporan');
+			redirect('home/index');
+		} elseif (!$this->upload->do_upload('lampiran')) { //Ketika eror ketika upload file
 			$error = array('error' => $this->upload->display_errors());
 			$this->load->view('templates/header',$data);
 			$this->load->view('home/form', $error);
 			$this->load->view('templates/footer'); 
-		} else {
+		} else { //Ketika tidak ada upload file
 			$uploadData = $this->upload->data();
 			$data = array(
 				'author' 	=> $nama,
@@ -89,7 +50,7 @@ class CRUD extends CI_Controller {
 			$this->Input_model->tambah($data,'laporan');
 			redirect('home/index');
 		}
-	}//akhir
+	}
 
 
 }
