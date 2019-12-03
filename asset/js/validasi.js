e46f198bec19da52db7007f7c80ea186f21e50ba
Arif wicak <ariftheaxr@gmail.com>
2019-12-03 15:40:59 +0700

    Keep validasi JS

--- asset/js/validasi.js
+++ asset/js/validasi.js
@@ -1,21 +1,27 @@
 function validasi() {
-	var text=document.forms["data"]["laporan"].value
-	var kategori = document.forms["data"]["aspek"].value
-	var lampiran = document.forms["data"]["lampiran"].value
-	//var salah = location.href = "http://localhost/IF3111-1920/home"
+	var judul		= document.forms["data"]["judul"].value;
+	var text		= document.forms["data"]["isi"].value;
+	var kategori 	= document.forms["data"]["aspek"].value;
+	var lampiran 	= document.forms["data"]["lampiran"].value;
 	//Hitung banyak kata
     var panjang=text.split(" ") 
 
-	// if (text == "") {
-	// 	alert("Isi laporan tidak boleh kosong")
-		
-	// } else if (panjang.length < 20) {
-	// 	alert("Isi laporan Minimal 20 kata")
-
-	// }
-	// if (kategori == "NULL") {
-	// 	alert("Pilih aspek laporan anda")
-
-	// }
+	// if (judul == "") {
+	// 	alert("Masukkan judul laporan anda");
+	// 	return false;
+	// } else if (text == "") {
+	// 	alert("Isi laporan tidak boleh kosong");
+	// 	return false;
+	// } else if (panjang.length < 2) {
+	// 	alert("Isi laporan Minimal 20 kata");
+	// 	return false;
+	// } else if (kategori == "NULL") {
+	// 	alert("Pilih aspek laporan anda");
+	// 	return false;
+	// } 
+	if (true) {
+		alert("DATA BERHASIL DI SIMPAN");
+	}
+	return(location.href ="http://localhost/IF3111-1920/home");
 	
 }
