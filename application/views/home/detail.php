8a02239d61b4693047cd03c3442a92fbecdda487
Arif wicak <ariftheaxr@gmail.com>
2019-12-14 23:21:07 +0700

    fix page detail

--- application/views/home/detail.php
+++ application/views/home/detail.php
@@ -1,18 +1,40 @@
 <!-- View Untuk Detail Laporan -->
+<script type="text/javascript" src="<?= base_url()?>asset/js/konfirmasi.js"></script>
 <div id="konten">
 	<p>Detail Laporan/Komentar </p>
-	<br><hr><br>
-	<?php foreach ($laporan as $value): ?>
-		<h1><?= $value['judul']; ?></h1>
+	<hr>
+	<?php foreach ($detail as $value): ?>
+		<?php $id = $value['ID']; ?>
+		<div class="wrapper">
+			<div class="gambar">
+				<img height="50px" src="<?= base_url()?>asset/icon/user.png">
+			</div>
+			<b> 
+			<?= $value['author']; ?> <br>
+			<span> Kategori : <?= $value['kategori']; ?></span>
+			</b>
+		</div>
+		<p> <?= $value['isi']; ?> </p>
 		<br>
-		<p style="text-align: justify;">
-		<?= $value['isi']; ?>
-		</p>
+		Lampiran : 
+		<br><br>
+		<a href="<?= base_url();?>upload/file/<?= $value['lampiran']; ?>">
+		<img style="height: 180px;" src="<?= base_url()?>asset/icon/lampiran.png">
 		<br>
-		<span>Lampiran : <?= $value['lampiran']; ?></span>
-		<br>
-		<p> Waktu: <?= $value['waktu']; ?> Aspek: <?= $value['kategori']; ?> 
-		<a href="<?= base_url();?>home/detail">Hapus Laporan/komentar<img height="20px" src="<?= base_url()?>asset/icon/silang.png"></a></p>
-		<br><hr>
+		<font style="font-size: 11pt"><?= $value['lampiran']; ?>	</font></a>
+		<br><br><br><br>
+		<div class="lampiran">
+			Waktu : <?= $value['waktu']; ?> &nbsp &nbsp&nbsp
+			Aspek : <?= $value['kategori']; ?>
+		</div>
+		<span>	
+			<p>
+				<?php echo "
+				<a href='http://localhost/IF3111-1920/Home/ubah?ubah_id=$id'>Ubah Laporan/Komentar<img style='height: 12px;' src='http://localhost/IF3111-1920/asset/icon/ubah.png'></a>  &nbsp
+				<a href=javascript:konfirmasi('http://localhost/IF3111-1920/CRUD/hapus?hapus_id=$id')>Hapus Laporan/komentar<img style='height: 15px;' src='http://localhost/IF3111-1920/asset/icon/silang.png'></a>
+				";?>
+			</p>
+		</span>
+		<hr>
 	<?php endforeach; ?>
 </div>
