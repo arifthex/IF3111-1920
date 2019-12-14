4e49499931f30a2a953ced92693314cfd9602b8c
Arif wicak <ariftheaxr@gmail.com>
2019-12-14 23:20:51 +0700

    fix model crud

--- application/models/Input_model.php
+++ application/models/Input_model.php
@@ -7,6 +7,15 @@ class Input_model extends CI_Model{
     public function tambah($data,$table){    
         return $this->db->insert($table, $data);
     }
-
+    public function update($where,$data,$table){  
+        $this->db->where($where);
+        $this->db->update($table,$data);
+    }
+    public function hapus(){    
+        $id = $_GET["hapus_id"];
+        $this->db->delete('laporan', array('ID' => $id));
+        echo "<script>alert('Data Berhasil dihapus')</script>";
+        echo "<script>location='http://localhost/IF3111-1920'</script>"; 
+    }
 }
 ?>
