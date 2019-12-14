43f91ff926ff55781ee8ce33ffd8ac79caf82a00
Arif wicak <ariftheaxr@gmail.com>
2019-12-14 23:20:32 +0700

    fix controller crud

--- application/controllers/CRUD.php
+++ application/controllers/CRUD.php
@@ -13,7 +13,6 @@ class CRUD extends CI_Controller {
 		date_default_timezone_set('Asia/jakarta'); //Menambah zona waktu asia jakarta
 		$data['title']='Buat Laporan';
 		$nama	= $this->input->post('author');
-		$judul	= $this->input->post('judul');
 		$isi	= $this->input->post('isi');
 		$aspek	= $this->input->post('aspek');
 		$waktu	= date('Y-m-d H:i:s'); //Format penulisan waktu sesuai database
@@ -29,20 +28,7 @@ class CRUD extends CI_Controller {
 			$nama = $samaran;
 		}
 
-		//Ketika lampiran di upload
-		if (! $_FILES['lampiran']['error'] <> 4) {
-			$uploadData = $this->upload->data();
-			$data = array(
-				'author' 	=> $nama,
-				'isi' 		=> $isi,
-				'judul' 	=> $judul,
-				'kategori' 	=> $aspek,
-				'lampiran' 	=> $uploadData['file_name'],
-				'waktu' 	=> $waktu
-			);
-			$this->Input_model->tambah($data,'laporan');
-			redirect('home/index');
-		} elseif (!$this->upload->do_upload('lampiran')) { //Ketika eror ketika upload file
+		if (!$this->upload->do_upload('lampiran')) { //Ketika eror ketika upload file
 			$error = array('error' => $this->upload->display_errors());
 			$this->load->view('templates/header',$data);
 			$this->load->view('home/form', $error);
@@ -52,7 +38,6 @@ class CRUD extends CI_Controller {
 			$data = array(
 				'author' 	=> $nama,
 				'isi' 		=> $isi,
-				'judul' 	=> $judul,
 				'kategori' 	=> $aspek,
 				'lampiran' 	=> $uploadData['file_name'],
 				'waktu' 	=> $waktu
@@ -61,6 +46,47 @@ class CRUD extends CI_Controller {
 			redirect('home/index');
 		}
 	}
+	public function hapus(){
+		$this->Input_model->hapus();
+	}
+	public function update(){
+		date_default_timezone_set('Asia/jakarta'); //Menambah zona waktu asia jakarta
+		$id		= $this->input->post('ID');
+		$nama	= $this->input->post('author');
+		$isi	= $this->input->post('isi');
+		$aspek	= $this->input->post('aspek');
+		$waktu	= date('Y-m-d H:i:s'); //Format penulisan waktu sesuai database
+		$samaran = "Anonymous";
 
+		$config['upload_path']          = 'upload/file/';
+		$config['allowed_types']        = 'doc|docx|xls|xlsx|ppt|pptx|pdf';
+		$config['max_size']             =  2048;
 
+		$this->load->library('upload', $config);
+
+		if ($nama == NULL) { //Ketika pelapor tidak memasukkan nama
+			$nama = $samaran;
+		}
+
+		if (!$this->upload->do_upload('lampiran')) { //Ketika eror ketika upload file
+			$error = array('error' => $this->upload->display_errors());
+			$this->load->view('templates/header',$data);
+			$this->load->view('home/form', $error);
+			$this->load->view('templates/footer'); 
+		} else { //Ketika tidak ada upload file
+			$uploadData = $this->upload->data();
+			$data = array(
+				'author' 	=> $nama,
+				'isi' 		=> $isi,
+				'kategori' 	=> $aspek,
+				'lampiran' 	=> $uploadData['file_name'],
+				'waktu' 	=> $waktu
+			);
+			$where = array(
+				'id' => $id
+			);
+			$this->Input_model->update($where,$data,'laporan');
+			redirect('home/index');
+		}
+	}
 }
