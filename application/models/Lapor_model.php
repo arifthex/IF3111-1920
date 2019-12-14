abe52dbf4389779960cffe31f9afbb6b27fe59b5
Arif wicak <ariftheaxr@gmail.com>
2019-12-14 23:20:59 +0700

    fix model

--- application/models/Lapor_model.php
+++ application/models/Lapor_model.php
@@ -11,11 +11,20 @@ class Lapor_model extends CI_Model{
 	public function search(){  
 		$key = $this->input->post('keyword');
 
-		$this->db->like('judul',$key);
-		$this->db->or_like('isi',$key);
+		$this->db->like('isi',$key);
 		$this->db->or_like('kategori',$key);
 		$this->db->order_by('waktu', 'DESC');
 		$data = $this->db->get('laporan');
+		return $data->result_array();  
+	}
+	public function detail(){  
+		$id = $_GET["detail"];
+		$data = $this->db->get_where('laporan', array('ID' => $id));
+		return $data->result_array();  
+	}
+	public function ubah(){  
+		$id = $_GET["ubah_id"];
+		$data = $this->db->get_where('laporan', array('ID' => $id));
 		return $data->result_array();  
 	}
 }
