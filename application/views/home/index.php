321f715cde0f273d1cd0a187fa00a06d0ffdab83
Arif wicak <ariftheaxr@gmail.com>
2019-12-14 14:54:38 +0700

    Update fitur beranda

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,7 +1,7 @@
 <!-- View pada Beranda Home Web  -->
 <div id="badan">
 	<form action="<?= base_url(); ?>Search" method="POST">
-		<input type="text" placeholder="Masukkan laporan yang ingin anda cari..." 
+		<input type="text" placeholder="Search..." 
 		name="keyword" id="keyword" autocomplete="off">
 		<input type="submit" name="cari" value="Cari" id="cari-data">
 	</form>
@@ -17,7 +17,7 @@
 		}
 		?>
 	</p>
-	<br><hr>
+	<hr>
 	<?php foreach ($laporan as $value): ?>
 		<div class="wrapper">
 			<div class="gambar">
@@ -28,29 +28,20 @@
 				<span>Kategori : <?= $value['kategori']; ?></span>
 			</b>
 		</div>
-		<h2><?= $value['judul']; ?></h2>
-		<br>
-		<p> <div id="readmore">
-			<?php 
-				$string 	= $value['isi'];
-				$karakter 	= strlen($string);
-				readmore($string,$karakter);
-			?>	
-			</div>
-			<div id="expand">
-				<?php expand($string); ?>
-			</div>
-		</p>
-		<br>
+		<p> <?= $value['isi']; ?> </p> 
+		<br><br><br>
 		<div class="lampiran">
-			<a href="<?= base_url();?>upload/file/<?= $value['lampiran']; ?>"><?= $value['lampiran']; ?></a>	
+			Lampiran : 
+			<a href="<?= base_url();?>upload/file/<?= $value['lampiran']; ?>"><?= $value['lampiran']; ?></a>
 		</div>
 		<span>
-			<p><?= $value['waktu']; ?> &nbsp <b>Lihat Selengkapnya ></b></p>
+			<p>Waktu : <?= $value['waktu']; ?>&nbsp<b><a href="">Lihat selengkapnya ></a></b></p>
 		</span>
 		<hr>
+		
 	<?php endforeach; ?>
 	<?php if ($laporan == null): ?>
 		<br><center> Data tidak ditemukan </center>
 	<?php endif; ?>
 </div> 
+<script src=" <?= base_url(); ?>asset/js/function.js"></script>
