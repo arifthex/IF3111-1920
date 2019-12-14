f510e7889290ab4e77fb49d45bc6d0e0bfd70812
Arif wicak <ariftheaxr@gmail.com>
2019-12-14 15:07:06 +0700

    Update perbaikan input laporan

--- asset/js/validasi.js
+++ asset/js/validasi.js
@@ -1,5 +1,4 @@
 function validasi() {
-	var judul		= document.forms["data"]["judul"].value;
 	var text		= document.forms["data"]["isi"].value;
 	var kategori 	= document.forms["data"]["aspek"].value;
 	var cekFile 	= document.forms["data"]["lampiran"].value;
@@ -8,14 +7,9 @@ function validasi() {
 	var ekstensiOk 	= /(.doc|.docx|.xls|.xlsx|.ppt|.ptx|.pdf)$/i;
 
 	//Hitung banyak kata
-    var panjang=text.split(" ") 
-    
-	if (judul == "") {
-		setTimeout(()=>{
-			window.location = "http://localhost/IF3111-1920/home/form"	
-		},0)
-		alert("Masukkan judul laporan anda");
-	} else if (text == "") {
+	var panjang=text.split(" ") 
+
+ 	if (text == "") {
 		setTimeout(()=>{
 			window.location = "http://localhost/IF3111-1920/home/form"	
 		},0)
@@ -30,17 +24,14 @@ function validasi() {
 			window.location = "http://localhost/IF3111-1920/home/form"	
 		},0)
 		alert("Pilih aspek laporan anda");
-	} else if (!cekFile  == "") {
-    	if(!ekstensiOk.exec(pathFile)){
-	        alert('Silakan upload file yang memiliki ekstensi\n\t     doc docx xls xlsx ppt ptx pdf');
-	        return false;  
-    	} else if (ekstensiOk.exec(pathFile)){
-    		alert("Data Berhasil Disimpan"); 
-    	} else {
-    		alert("Kesalahan Upload File"); 
-    	}
-    } else {
-    	alert("Data Berhasil Disimpan"); 	
-    }
-	
+	} else if(!ekstensiOk.exec(pathFile)){
+		setTimeout(()=>{
+			window.location = "http://localhost/IF3111-1920/home/form"	
+		},0)
+		alert('Silakan upload file yang memiliki ekstensi\n\t     doc docx xls xlsx ppt ptx pdf');
+		return false;  
+	} else {
+		alert("Data Berhasil Disimpan"); 	
+	}
+
 }
